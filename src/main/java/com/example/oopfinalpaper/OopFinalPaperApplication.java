233bc57cf9068e7aeebbb49ce2d8cca3fe8fcad0
laiba-107 <alilaiba830@gmail.com>
2024-12-20 00:07:52 +0600

    init changes

---
+++ src/main/java/com/example/oopfinalpaper/OopFinalPaperApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopfinalpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopFinalPaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopFinalPaperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oopfinalpaper/OopFinalPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopfinalpaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopFinalPaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
