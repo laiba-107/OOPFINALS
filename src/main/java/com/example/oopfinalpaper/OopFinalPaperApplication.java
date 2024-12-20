83c6745c8cb3ef3e27634ece1809992da51e6d10
laiba-107 <alilaiba830@gmail.com>
2024-12-20 11:23:04 +0600

    added

--- src/main/java/com/example/oopfinalpaper/OopFinalPaperApplication.java
+++ src/main/java/com/example/oopfinalpaper/OopFinalPaperApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OopFinalPaperApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OopFinalPaperApplication.class, args);
     }
-
 }
