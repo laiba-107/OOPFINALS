661d9bb0a47af8f365e600905a394c3f5c0be60f
laiba-107 <alilaiba830@gmail.com>
2024-12-20 12:48:32 +0600

    added change

--- src/main/java/com/example/oopfinalpaper/model/Event.java
+++ src/main/java/com/example/oopfinalpaper/model/Event.java
@@ -1,21 +1,75 @@
 package com.example.oopfinalpaper.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
 
-@Data
 @Entity
 public class Event {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-    private LocalDateTime dateTime;
+
     private String location;
 
+    private LocalDateTime dateTime;
+
     private int ticketsAvailable;
+
     private int ticketsSold;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getTicketsAvailable() {
+        return ticketsAvailable;
+    }
+
+    public void setTicketsAvailable(int ticketsAvailable) {
+        this.ticketsAvailable = ticketsAvailable;
+    }
+
+    public int getTicketsSold() {
+        return ticketsSold;
+    }
+
+    public void setTicketsSold(int ticketsSold) {
+        this.ticketsSold = ticketsSold;
+    }
 }
